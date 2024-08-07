4343c2fac5672e84ddd729225c4271a1723b71a6
Mac <mac@Macs-MacBook-Pro.local>
2024-08-07 23:17:08 +0530

    initial commits

---
+++ AppLifeCycle/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Mac on 07/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+//when app launch initially these methods are called
+
+//didFinishLaunchingWithOptions
+//configurationForConnecting
+//willConnectTo
+//sceneWillEnterForeground
+//sceneDidBecomeActive
+
+
+//when temporary call is happen
+
+//sceneWillResignActive
+
+
+//if we tap on home button
+
+//SceneWillResignActive
+//sceneDidEnterBackground
+
+
+//if we again open the app
+
+//sceneWillEnterForeground
+//sceneDidBecomeActive
+
